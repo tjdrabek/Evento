6cbbcfb6e3f0afbd5c5bf3696e7ea098df735e27
Tyler Drabek <tjdrabek@gmail.com>
2023-12-09 13:36:33 -0700

    add content to the home page and learn some Tailwind tricks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header>
+      <Logo />
       <Link href="/">Home</Link>
       <Link href="/events/all">All Events</Link>
     </header>
