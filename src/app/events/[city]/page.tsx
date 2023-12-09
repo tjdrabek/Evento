36bef6967e16762b909c40a27bb9be48d8c6fef3
Tyler Drabek <tjdrabek@gmail.com>
2023-12-09 16:24:32 -0700

    extract a reusable h1 component

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,3 +1,9 @@
+import H1 from "@/components/h1";
+
 export default function EventsPage() {
-  return <div>EventsPage</div>;
+  return (
+    <main>
+      <H1>Events in Austin</H1>
+    </main>
+  );
 }
