b4f146f9c2fa9531dea8931c90cc846c4fe786e7
Tyler Drabek <tjdrabek@gmail.com>
2023-12-11 00:04:58 -0700

    added missing return keyword in cn utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,5 +2,5 @@ import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
