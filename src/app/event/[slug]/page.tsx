3888931ae8c36eaed3d6ab518e16156cc41dd7cf
Tyler Drabek <tjdrabek@gmail.com>
2023-12-11 09:15:57 -0700

    implement the overall structure of the event page

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 type EventPageProps = {
   params: {
     slug: string;
@@ -10,5 +12,12 @@ export default async function EventPage({ params }: EventPageProps) {
     `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
   );
   const event = await response.json();
-  return <main>Event page</main>;
+  return (
+    <main>
+      <section>
+        <Image />
+      </section>
+      <div></div>
+    </main>
+  );
 }
