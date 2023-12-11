4e9b3934450ef7f4cc28071d535943e1b9c39dd4
Tyler Drabek <tjdrabek@gmail.com>
2023-12-10 23:24:21 -0700

    add the event date to the event card

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -7,7 +7,7 @@ type EventCardProps = {
 
 export default function EventCard({ event }: EventCardProps) {
   return (
-    <section className="flex flex-col flex-1 basis-80 h-[380px] max-w-[500px] bg-white/[3%] rounded-xl overflow-hidden">
+    <section className="flex flex-col flex-1 basis-80 h-[380px] max-w-[500px] bg-white/[3%] rounded-xl overflow-hidden relative">
       <Image
         src={event.imageUrl}
         alt={event.name}
@@ -20,6 +20,15 @@ export default function EventCard({ event }: EventCardProps) {
         <p className="italic text-white/75">By {event.organizerName}</p>
         <p className="text-sm text-white/50 mt-4">{event.location}</p>
       </div>
+
+      <section className="absolute flex flex-col justify-center items-center left-[12-px] top-[12px] h-[45px] w-[45px] bg-black/30 rounded-md">
+        <p className="text-xl font-bold -mb-[5px]">
+          {new Date(event.date).toLocaleDateString("en-US", {
+            day: "2-digit",
+          })}
+        </p>
+        <p className="text-xs uppercase text-accent">Nov</p>
+      </section>
     </section>
   );
 }
