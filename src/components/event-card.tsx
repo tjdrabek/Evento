87596463a0bbab4487c3cab1d0d764332dd3eaa6
Tyler Drabek <tjdrabek@gmail.com>
2023-12-11 11:43:13 -0700

    extract hover effect to a reusable class

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -12,7 +12,7 @@ export default function EventCard({ event }: EventCardProps) {
       className="flex-1 basis-80 h-[380px] max-w-[500px]"
       href={`/event/${event.slug}`}
     >
-      <section className="w-full h-full flex flex-col bg-white/[3%] rounded-xl overflow-hidden relative transition hover:scale-105 active:scale-[1.02]">
+      <section className="w-full h-full flex flex-col bg-white/[3%] rounded-xl overflow-hidden relative state-effects">
         <Image
           src={event.imageUrl}
           alt={event.name}
