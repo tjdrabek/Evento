5885688500c6a55c880bdb1e91aef8e4e707d4cb
Tyler Drabek <tjdrabek@gmail.com>
2023-12-10 22:47:18 -0700

    restructure to use a dedicated events list component

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,3 +1,4 @@
+import EventsList from "@/components/events-list";
 import H1 from "@/components/h1";
 import { EventoEvent } from "@/lib/types";
 
@@ -8,13 +9,11 @@ type EventsPageProps = {
 };
 
 export default async function EventsPage({ params }: EventsPageProps) {
-  const city = params.city;
-
   const respnse = await fetch(
     "https://bytegrad.com/course-assets/projects/evento/api/events?city=austin"
   );
   const events: EventoEvent[] = await respnse.json();
-  console.log(events);
+  const city = params.city;
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
@@ -23,9 +22,7 @@ export default async function EventsPage({ params }: EventsPageProps) {
         {city !== "all" &&
           `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
       </H1>
-      {events.map((event) => (
-        <section key={event.id}>{event.name}</section>
-      ))}
+      <EventsList events={events} />
     </main>
   );
 }
