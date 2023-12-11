b24aaaf25e55ab6c64b728241114c02da3e2b67d
Tyler Drabek <tjdrabek@gmail.com>
2023-12-11 09:35:42 -0700

    add advanced options for the image component

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -14,8 +14,24 @@ export default async function EventPage({ params }: EventPageProps) {
   const event = await response.json();
   return (
     <main>
-      <section>
-        <Image />
+      <section className="relative h-[361px] overflow-hidden">
+        <Image
+          className="object-cover z-0 blur-3xl"
+          src={event.imageUrl}
+          alt="Event background image"
+          fill
+          quality={50}
+          sizes="(max-width:1280px) 100vw, 1280px"
+        />
+        <div>
+          <Image
+            className="z-1 relative"
+            src={event.imageUrl}
+            alt={event.name}
+            width={300}
+            height={201}
+          />
+        </div>
       </section>
       <div></div>
     </main>
