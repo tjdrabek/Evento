0684e51510687144667aa5ac4563d23036cf2aa1
Tyler Drabek <tjdrabek@gmail.com>
2023-12-11 09:04:29 -0700

    get event slug from url

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,12 @@
-export default function EventPage() {
+type EventPageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function EventPage({ params }: EventPageProps) {
+  const response = await fetch(
+    `http://localhost:3000/api/events/${params.slug}`
+  );
   return <main>Event page</main>;
 }
