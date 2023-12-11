004c6364c6338a9770702a034bb8fae0d4ed321c
Tyler Drabek <tjdrabek@gmail.com>
2023-12-11 10:24:56 -0700

    finish intro section of the event page

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import H1 from "@/components/h1";
 import Image from "next/image";
 
 type EventPageProps = {
@@ -14,23 +15,42 @@ export default async function EventPage({ params }: EventPageProps) {
   const event = await response.json();
   return (
     <main>
-      <section className="relative h-[361px] overflow-hidden">
+      <section className="relative overflow-hidden flex justify-center items-center py-14 md:py-20">
         <Image
-          className="object-cover z-0 blur-3xl"
           src={event.imageUrl}
+          className="object-cover z-0 blur-3xl"
           alt="Event background image"
           fill
           quality={50}
           sizes="(max-width:1280px) 100vw, 1280px"
         />
-        <div>
+        <div className="z-1 flex flex-col gap-6 lg:gap-16 lg:flex-row relative">
           <Image
-            className="z-1 relative"
             src={event.imageUrl}
             alt={event.name}
             width={300}
             height={201}
+            className="rounded-xl border-2 border-white/50 object-cover"
           />
+          <div className="flex flex-col">
+            <p className="text-white/75">
+              {new Date(event.date).toLocaleDateString("en-US", {
+                weekday: "long",
+                month: "long",
+                day: "numeric",
+              })}
+            </p>
+            <p>{event.name}</p>
+            <H1 className="mb-2 mt-1 whitespace-nowrap lg:text-5xl">
+              {event.name}
+            </H1>
+            <p className="whitespace-nowrap text-xl text-white/75">
+              Organized by <span className="italic">{event.organizerName}</span>
+            </p>
+            <button className="bg-white/20 text-lg capitalize bg-blur mt-5 lg:mt-auto w-[95vw] rounded-md border-white/10 border-2 sm:w-full py-2 hover:scale-105 focus:scale-105 active:scale-[1.02] transition">
+              Get tickets
+            </button>
+          </div>
         </div>
       </section>
       <div></div>
