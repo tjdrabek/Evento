383ab446135605718d3190bf2c519273111e909c
Tyler Drabek <tjdrabek@gmail.com>
2023-12-18 09:07:15 -0700

    create data fetching utilities to keep code DRY

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import H1 from "@/components/h1";
+import { getEvent } from "@/lib/utils";
 import { Metadata } from "next";
 import Image from "next/image";
 import React from "react";
@@ -11,10 +12,7 @@ type Props = {
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const slug = params.slug;
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
-  );
-  const event = await response.json();
+  const event = await getEvent(slug);
 
   return {
     title: event.name,
@@ -23,10 +21,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
 export default async function EventPage({ params }: Props) {
   const slug = params.slug;
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
-  );
-  const event = await response.json();
+  const event = await getEvent(slug);
   return (
     <main>
       <section className="relative overflow-hidden flex justify-center items-center py-14 md:py-20">
--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -1,21 +1,12 @@
-import { EventoEvent } from "@/lib/types";
 import EventCard from "./event-card";
-import { sleep } from "@/lib/utils";
+import { getEvents } from "@/lib/utils";
 
 type EventsListProps = {
   city: string;
 };
 
 export default async function EventsList({ city }: EventsListProps) {
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`,
-    {
-      next: {
-        revalidate: 300,
-      },
-    }
-  );
-  const events: EventoEvent[] = await response.json();
+  const events = await getEvents(city);
 
   return (
     <section className="max-w-[1100px] flex flex-wrap gap-10 justify-center px-[20px]">
