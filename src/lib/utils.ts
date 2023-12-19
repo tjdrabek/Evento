c86cf17e6958e6c187526239086614a43edea028
Tyler Drabek <tjdrabek@gmail.com>
2023-12-19 12:20:32 -0700

    route to not found page when invalid slug is passed

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import prisma from "./db";
+import { notFound } from "next/navigation";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -29,5 +30,8 @@ export async function getEvents(city: string) {
 
 export async function getEvent(slug: string) {
   const event = await prisma.eventoEvent.findUnique({ where: { slug } });
+  if (!event) {
+    return notFound();
+  }
   return event;
 }
