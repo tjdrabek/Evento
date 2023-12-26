b11da22ab11bb7b4f75ce24451ecfaba6cef98a5
Tyler Drabek <tjdrabek@gmail.com>
2023-12-26 12:05:31 -0700

    retrigger the suspense component with fallback loading when searchParams change

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -36,7 +36,7 @@ export default async function EventsPage({
         {city === "all" && "All Events"}
         {city !== "all" && `Events in ${capitalize(city)}`}
       </H1>
-      <Suspense fallback={<Loading />}>
+      <Suspense key={city + page} fallback={<Loading />}>
         <EventsList city={city} page={+page} />
       </Suspense>
     </main>
