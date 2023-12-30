107915c786519b317691d2d1f7b53783ecd8564e
Tyler Drabek <tjdrabek@gmail.com>
2023-12-29 21:56:18 -0700

    statically render some popular routes

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -19,6 +19,18 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   };
 }
 
+export async function generateStaticParams() {
+  // top 100 most popular events
+  return [
+    {
+      slug: "comedy-extravaganza",
+    },
+    {
+      slug: "dj-practice-session",
+    },
+  ];
+}
+
 export default async function EventPage({ params }: Props) {
   const slug = params.slug;
   const event = await getEvent(slug);
